cf22d2c608147eae17f13473aa625ac011d53a42
Mona Lisa <romain-hd.perez@live.fr>
2020-04-30 14:51:02 +0200

    void

--- src/main/java/fr/epsi/mspr/recycl/controller/IotController.java
+++ src/main/java/fr/epsi/mspr/recycl/controller/IotController.java
@@ -3,10 +3,7 @@ package fr.epsi.mspr.recycl.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -16,9 +13,9 @@ public class IotController {
     ArrayList<String> test = new ArrayList<>();
 
     @PostMapping("/greeting")
-    public HttpStatus greeting(@RequestBody String body) {
+    @ResponseStatus(HttpStatus.OK)
+    public void greeting(@RequestBody String body) {
         System.out.println(body);
-        return HttpStatus.OK;
     }
 
 }
